6395f46f3298da065d9c87ba9fb43ba7b7b9df84
Elijah Joseph-Young <eli.jy94@gmail.com>
2024-01-20 16:43:56 -0500

    ability to add excercise

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { useState } from "react";
+
 function App() {
+  const [excercises, setExcercises] = useState([]);
+  const [excercise, setExcercise] = useState('');
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setExcercises([...excercises, excercise])
+    setExcercise('');
+  }
+
   return (
     <div>
       Training App
+      <form onSubmit={submitHandler}>
+        <label>Excercise: </label>
+        <input value={excercise} onChange={(e) => setExcercise(e.target.value)}/>
+        <button>Add</button>
+      </form>
+      {excercises.map(excercise => {
+        return <div>{excercise}</div>
+      })}
     </div>
   );
 }
