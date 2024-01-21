7274b1ac8eee6a8b81b551e0a918871f19aa1c37
Elijah Joseph-Young <16768157+elijy@users.noreply.github.com>
2024-01-20 20:40:24 -0500

    add persistence with json-server

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Exercise from "./components/Exercise";
 
 function App() {
   const [exercises, setExercises] = useState([]);
   const [exercise, setExercise] = useState("");
 
-  const submitHandler = (e) => {
+  const fetchExercises = async () => {
+    const { data } = await axios.get("http://localhost:3001/exercises");
+    setExercises([...data]);
+  };
+
+  useEffect(() => {
+    fetchExercises();
+  }, []);
+
+  const submitHandler = async (e) => {
     e.preventDefault();
-    setExercises([
-      ...exercises,
-      { id: Math.floor(Math.random() * 999), name: exercise, sets: [] },
-    ]);
+    const { data } = await axios.post("http://localhost:3001/exercises", {
+      name: exercise,
+      sets: [],
+    });
+    setExercises([...exercises, { ...data }]);
     setExercise("");
   };
 
-  const addSetsReps = (id, weight, reps) => {
-    setExercises([
-      ...exercises.map((ex) =>
-        ex.id === id ? { ...ex, sets: [...ex.sets, { weight, reps }] } : ex
-      ),
-    ]);
+  const addSetsReps = async (id, weight, reps) => {
+    const exToBeUpdated = exercises.find((ex) => ex.id === id);
+    const { data } = await axios.put(`http://localhost:3001/exercises/${id}`, {
+      ...exToBeUpdated,
+      sets: [...exToBeUpdated.sets, { weight, reps }],
+    });
+
+    setExercises([...exercises.map((ex) => (ex.id === id ? { ...data } : ex))]);
   };
 
   return (
