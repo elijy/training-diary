98a72e3e0e3a0e4cc62650dfdbcc03eeb20c42f7
Elijah Joseph-Young <16768157+elijy@users.noreply.github.com>
2024-02-03 15:07:10 -0500

    remove unused context

--- src/hooks/use-exercises-context.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import ExerciseContext from "../context/exercises";
-
-function useExcercisesContext() {
-  return useContext(ExerciseContext);
-}
-
-export default useExcercisesContext;
