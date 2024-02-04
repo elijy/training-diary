505eeecb2715a8cda2058ef8848fb51794d94ad6
Elijah Joseph-Young <16768157+elijy@users.noreply.github.com>
2024-02-04 14:07:37 -0500

    remove unused files

--- src/store/thunks/deleteSet.js
+++
@@ -1,7 +0,0 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
-
-export const deleteSet = createAsyncThunk("set/delete", async (id) => {
-  await axios.delete(`http://localhost:3001/sets/${id}`);
-  return id;
-});
--- src/store/thunks/fetchExercises.js
+++
@@ -1,7 +0,0 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
-
-export const fetchExercises = createAsyncThunk("exercises/fetch", async () => {
-  const { data } = await axios.get("http://localhost:3001/exercises");
-  return data;
-});
