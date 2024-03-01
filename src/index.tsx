44714a69f9bbd27066b4ec95b0d22969a43cba90
Elijah Joseph-Young <16768157+elijy@users.noreply.github.com>
2024-03-01 18:13:12 -0500

    add apollo to app

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,23 @@
 import { createRoot } from "react-dom/client";
 import { Provider as ReduxProvider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 import store from "./store";
 import router from "./router";
 
+import { baseUrl } from "./store/apis/config";
+
+const client = new ApolloClient({
+  uri: `${baseUrl}/graphql`,
+  cache: new InMemoryCache(),
+});
+
 const root = createRoot(document.getElementById("root"));
 root.render(
-  <ReduxProvider store={store}>
-    <RouterProvider router={router} />
-  </ReduxProvider>
+  <ApolloProvider client={client}>
+    <ReduxProvider store={store}>
+      <RouterProvider router={router} />
+    </ReduxProvider>
+  </ApolloProvider>
 );
