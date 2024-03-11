215a3916205e20577c55e882d2f0fe2cab350dac
Elijah Joseph-Young <16768157+elijy@users.noreply.github.com>
2024-03-11 18:53:35 -0400

    delete workout mutation

---
+++ src/queries/deleteWorkout.ts
@@ -0,0 +1,10 @@
+import { gql } from "@apollo/client";
+
+export const DELETE_WORKOUT = gql`
+  mutation deleteWorkout($id: String!) {
+    deleteWorkout(id: $id) {
+      id
+      date
+    }
+  }
+`;
