a08e33b0c0952445fde91316aaa09ed6fbc4f321
Elijah Joseph-Young <16768157+elijy@users.noreply.github.com>
2024-03-20 19:03:41 -0400

    move add workout to a card

--- src/router/routes/WorkoutOverview.tsx
+++ src/router/routes/WorkoutOverview.tsx
@@ -1,20 +1,34 @@
-import { useNavigate } from "react-router-dom";
 import { useQuery } from "@apollo/client";
+import { useMutation } from "@apollo/client";
 
-import { GET_WORKOUTS } from "../../queries/getWorkouts";
+import { GET_WORKOUTS, ADD_WORKOUT } from "../../queries";
+
+import Card from "../../components/core/Card";
+import { IoIosAddCircle } from "react-icons/io";
 
 import { Workout } from "../../types/Workout";
 
 import WorkoutDetail from "../../components/WorkoutDetail";
 
 function WorkoutOverview() {
-  const navigate = useNavigate();
+  const [createWorkout] = useMutation(ADD_WORKOUT, {
+    refetchQueries: [{ query: GET_WORKOUTS }],
+  });
 
   const { loading, data }: { loading: Boolean; data: { workouts: Workout[] } } =
     useQuery(GET_WORKOUTS);
 
   const handleClick = () => {
-    navigate("/workouts/new");
+    createWorkout({ variables: { date: new Date() } });
+  };
+
+  const newWorkoutCard = () => {
+    return (
+      <div className="is-flex is-flex-direction-column is-align-items-center is-justify-content-center">
+        <div className="subtitle">Add a new workout</div>
+        <IoIosAddCircle className="icon is-large" />
+      </div>
+    );
   };
 
   // TODO: Add skeleton loading component
@@ -23,25 +37,18 @@ function WorkoutOverview() {
   }
 
   return (
-    <div>
-      <div className="block">
-        <button
-          className="button is-large is-primary is-rounded is-fullwidth"
-          onClick={handleClick}
-        >
-          Start a new workout
-        </button>
-      </div>
-      <div className="block">
-        <div className="columns is-multiline">
-          {data?.workouts?.map((workout) => {
-            return (
-              <div className="column is-one-quarter" key={workout.id}>
-                <WorkoutDetail workout={workout} />
-              </div>
-            );
-          })}
+    <div className="block">
+      <div className="columns is-multiline">
+        <div className="column is-one-quarter">
+          <Card onClick={handleClick} content={newWorkoutCard()} />
         </div>
+        {data?.workouts?.map((workout) => {
+          return (
+            <div className="column is-one-quarter" key={workout.id}>
+              <WorkoutDetail workout={workout} />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
