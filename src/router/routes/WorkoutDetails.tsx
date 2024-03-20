2955b282f7be947a6b36150246f9fc986f0c0ad5
Elijah Joseph-Young <16768157+elijy@users.noreply.github.com>
2024-03-20 19:05:59 -0400

    remove createworkout code

--- src/router/routes/WorkoutDetails.tsx
+++ src/router/routes/WorkoutDetails.tsx
@@ -1,33 +1,15 @@
 import ExerciseList from "../../components/ExerciseList";
 import ExerciseCreate from "../../components/ExerciseCreate";
 
-import { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { useMutation } from "@apollo/client";
-
-import { ADD_WORKOUT } from "../../queries/addWorkout";
-import { GET_WORKOUTS } from "../../queries/getWorkouts";
 
 function WorkoutDetails() {
   const { workoutId } = useParams();
-  const [createWorkout, { data }] = useMutation(ADD_WORKOUT, {
-    refetchQueries: [{ query: GET_WORKOUTS }],
-  });
-
-  useEffect(() => {
-    if (workoutId === "new") {
-      createWorkout({ variables: { date: new Date() } });
-    }
-  }, [workoutId, createWorkout]);
-
-  const getWorkoutId = () => {
-    return workoutId === "new" ? data?.id : workoutId;
-  };
 
   return (
     <div>
-      <ExerciseCreate workoutId={getWorkoutId()} />
-      <ExerciseList workoutId={getWorkoutId()} />
+      <ExerciseCreate workoutId={workoutId} />
+      <ExerciseList workoutId={workoutId} />
     </div>
   );
 }
