6c7d02e650abbc4c5138774f7fe17415ccb1ceed
Elijah Joseph-Young <16768157+elijy@users.noreply.github.com>
2024-04-09 18:51:38 -0400

    update add sets query

--- src/queries/addSet.ts
+++ src/queries/addSet.ts
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
 
 export const ADD_SET = gql`
-  mutation addSet($exerciseId: String!, $weight: Int, $reps: Int) {
+  mutation addSet($exerciseId: Int!, $weight: Int, $reps: Int) {
     addSet(exerciseId: $exerciseId, weight: $weight, reps: $reps) {
       id
       weight
