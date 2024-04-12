8c7debf9665fc82ab3f2a605d21d393587adb616
Elijah Joseph-Young <16768157+elijy@users.noreply.github.com>
2024-04-12 19:36:02 -0400

    edit exercise query update

--- src/queries/editExercise.ts
+++ src/queries/editExercise.ts
@@ -1,8 +1,8 @@
 import { gql } from "@apollo/client";
 
 export const EDIT_EXERCISE = gql`
-  mutation editExercise($id: String!, $workoutId: String!, $name: String!) {
-    editExercise(id: $id, workoutId: $workoutId, name: $name) {
+  mutation editExercise($id: Int!, $name: String!) {
+    editExercise(id: $id, name: $name) {
       id
       name
     }
